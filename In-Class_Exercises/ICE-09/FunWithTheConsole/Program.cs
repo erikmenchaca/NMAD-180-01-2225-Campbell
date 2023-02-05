968e2e86df2fea84267460b52554302d062b48c0
erikmenchaca <em9927@rit.edu>
2023-02-05 18:15:14 -0500

    Initial Commit

---
+++ In-Class_Exercises/ICE-09/FunWithTheConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿//Name: Erik Menchaca
+//Date: February 5, 2023
+//Course: Programming Fundamentals I: Mobile Domain
+//Instructor: Kemoy Campbell
+//Assignment: Fun With The Console
+
+Console.Title = "Fun With The Console";
+System.Console.Write("What is your name? --> ");
+string name = Console.ReadLine();
+System.Console.Write("What is your major? --> ");
+string major = Console.ReadLine();
+System.Console.Write("What is your dream job? --> ");
+string dreamJob = Console.ReadLine();
+
+System.Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.WriteLine("Programming Fundamentals I: Mobile Domain");
+System.Console.Write($"Name: {name}, Major: {major}, Dream Job: {dreamJob}");
+
+Console.ResetColor();
